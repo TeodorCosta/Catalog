bbe326a895d40a57c1c4ab893e689afc956db416
TeodorCosta <teodor.ioan2412@gmail.com>
2024-06-07 14:22:03 +0300

    Fixed Thymeleaf templates

--- catalog/src/main/java/com/catalogApp/catalog/controller/MainController.java
+++ catalog/src/main/java/com/catalogApp/catalog/controller/MainController.java
@@ -4,12 +4,21 @@ package com.catalogApp.catalog.controller;
 import com.catalogApp.catalog.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
     @Autowired
     StudentService studentService;
 
+    @GetMapping("/home")
+    public String index(){
+        return" index";
+    }
+    @GetMapping("/nav")
+    public String nav(){
+        return"nav_bar";
+    }
 
 
 }
