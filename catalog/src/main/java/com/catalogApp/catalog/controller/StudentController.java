bbca7fadcdead1387d978d89446f2c6bf9613cd0
TeodorCosta <teodor.ioan2412@gmail.com>
2024-06-07 17:05:04 +0300

    Angajat-Form Backend

---
+++ catalog/src/main/java/com/catalogApp/catalog/controller/StudentController.java
@@ -0,0 +1,28 @@
+package com.catalogApp.catalog.controller;
+
+import com.catalogApp.catalog.entity.Student;
+import com.catalogApp.catalog.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class StudentController {
+
+    @Autowired
+    StudentService studentService;
+
+    @GetMapping("/student_form")
+    public String studentForm(Model model) {
+        model.addAttribute("student", new Student());
+        return "student-form";
+    }
+    @PostMapping("/save_student")
+    public String saveStudent(Student student) {
+       studentService.saveStudent(student);
+
+        return ("redirect:/nav");
+    }
+}
